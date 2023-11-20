134bbbff3b70b115d918d075320fffa593f8e785
Jeff <jeffersoncandidoeng@gmail.com>
2023-11-20 03:17:02 -0300

    First build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "export",
+    distDir: ".dist",
+    images: { unoptimized: true },
+    trailingSlash: true
+  };
 
 module.exports = nextConfig
